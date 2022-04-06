199d297d8f17ae82405a8f900b349f5d565baa18
AVRDevlepor <97604500+AVRDevlepor@users.noreply.github.com>
2022-04-06 14:45:07 +0100

    Update SourceCode.cs

--- SourceCode.cs
+++ SourceCode.cs
@@ -1,10 +1,56 @@
+using BepInEx;
+using System;
+using UnityEngine;
+using Utilla;
+
+namespace HereAreMod
+{
+    /// <summary>
+    /// This is your mod's main class.
+    /// </summary>
+
+    /* This attribute tells Utilla to look for [ModdedGameJoin] and [ModdedGameLeave] */
+    [ModdedGamemode]
+    [BepInDependency("org.legoandmars.gorillatag.utilla", "1.5.0")]
+    [BepInPlugin(PluginInfo.GUID, PluginInfo.Name, PluginInfo.Version)]
+    public class Plugin : BaseUnityPlugin
+    {
+        public bool inRoom;
+
+        void OnEnable()
+        {
+            /* Set up your mod here */
+            /* Code here runs at the start and whenever your mod is enabled*/
+
+            HarmonyPatches.ApplyHarmonyPatches();
+            Utilla.Events.GameInitialized += OnGameInitialized;
+        }
+
+        void OnDisable()
+        {
+            /* Undo mod setup here */
+            /* This provides support for toggling mods with ComputerInterface, please implement it :) */
+            /* Code here runs whenever your mod is disabled (including if it disabled on startup)*/
+
+            HarmonyPatches.RemoveHarmonyPatches();
+            Utilla.Events.GameInitialized -= OnGameInitialized;
+        }
+
+        void OnGameInitialized(object sender, EventArgs e)
+        {
+            /* Code here runs after the game initializes (i.e. GorillaLocomotion.Player.Instance != null) */
+        }
+
         void Update()
         {
             /* Code here runs every frame when the mod is enabled */
-            if (inRoom && GorillaLocomotion.Player.Instance.GetComponent<Rigidbody>().useGravity)
+            if (inRoom)
             {
-                GorillaLocomotion.Player.Instance.GetComponent<Rigidbody>().AddForce(new Vector3(0f,5.2f,0f),ForceMode.Acceleration);
-                // Low gravity YAAAAAAY
+                if (GorillaLocomotion.Player.Instance.GetComponent<Rigidbody>().useGravity == true)
+                {
+                    GorillaLocomotion.Player.Instance.GetComponent<Rigidbody>().AddForce(new Vector3(0f, 5.2f, 0f), ForceMode.Acceleration);
+                }
+                    // Low gravity YAAAAAAY
 
             }
         }
@@ -28,3 +74,5 @@
 
             inRoom = false;
         }
+    }
+}
